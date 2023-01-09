5fae406132b37b512c970458d3a7b4b1df93d1d2
altarulas <altar@brewww.com>
2023-01-09 21:53:27 +0300

    commit: add firebae auth, storage and db

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -8,14 +10,16 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDYgl7CKVkVmkJrVZ2aPSjb8yJJzu8aAqQ",
-  authDomain: "chat-app-react-de766.firebaseapp.com",
-  projectId: "chat-app-react-de766",
-  storageBucket: "chat-app-react-de766.appspot.com",
-  messagingSenderId: "302844758741",
-  appId: "1:302844758741:web:834661d89991827144305c",
+  apiKey: "AIzaSyB4_Ew8tfW5359c0MH1ZBybv4qvmNzIB8I",
+  authDomain: "chat-app-react-b03d9.firebaseapp.com",
+  projectId: "chat-app-react-b03d9",
+  storageBucket: "chat-app-react-b03d9.appspot.com",
+  messagingSenderId: "269784705732",
+  appId: "1:269784705732:web:26b7b8d12633c4229af791",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
