90a6370d59f1f923e06fe74b5c5d477773bdfa09
altarulas <altar@brewww.com>
2023-01-11 19:48:03 +0300

    commit: add uuid package

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./style.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { AuthContextProvider } from "./Context/Auth";
+import { ChatContextProvider } from "./Context/Chat";
 import Home from "./Pages/Home";
 import { HomeProtection } from "./Context/ProtectedRoute";
 import Login from "./Pages/Login";
@@ -11,15 +12,17 @@ import Register from "./Pages/Register";
 function App() {
   return (
     <AuthContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<HomeProtection />}>
-            <Route path="/home" element={<Home />} />
-          </Route>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
+      <ChatContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<HomeProtection />}>
+              <Route path="/home" element={<Home />} />
+            </Route>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </ChatContextProvider>
     </AuthContextProvider>
   );
 }
--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,13 +1,18 @@
 import Add from "../Images/add.png";
 import Cam from "../Images/cam.png";
+import { ChatContext } from "../Context/Chat";
 import Input from "./Input";
 import Messages from "./Messages";
 import More from "../Images/more.png";
+import { useContext } from "react";
 
 const Chat = () => {
+    const { data } = useContext(ChatContext);
+
     return (
         <div className="chat">
             <div className="chatInfo">
+                {<span>{data.user?.displayName}</span>}
                 <div className="chatIcons">
                     <img src={Cam} alt="" />
                     <img src={Add} alt="" />
