3eeb134ed4777c75ad171af852c5f8904f949dbb
altarulas <altar@brewww.com>
2023-01-11 19:50:39 +0300

    commit: Add all the necessary code to the components

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,14 +1,104 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+
+import { AuthContext } from "../Context/Auth"
+import { db } from "../firebase"
 
 const Search = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  const [userName, setUserName] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const handleSelect = async () => {
+    //check whether the group(chats in firestore) exists, if not create
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        //create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      setError(true);
+    }
+
+    setUser(null);
+    setUserName("")
+  };
+
+  const handleSearch = async () => {
+    const q = query(collection(db, "users"), where("displayName", "==", userName));
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setError(true);
+    }
+
+  }
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
         <input
           type="text"
           placeholder="Find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUserName(e.target.value)}
+          value={userName}
         />
       </div>
+      {error && <span>User not found!</span>}
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
