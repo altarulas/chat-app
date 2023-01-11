fd268d2add1c52dfc0a4da3e63910148660d198c
altarulas <altarulas09@gmail.com>
2023-01-11 14:52:52 +0300

    commit: make home page route protection

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { AuthContextProvider } from "./Context/Auth";
 import Home from "./Pages/Home";
+import { HomeProtection } from "./Context/ProtectedRoute";
 import Login from "./Pages/Login";
-import ProtectedRoute from "./Context/ProtectedRoute";
 import Register from "./Pages/Register";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
     <AuthContextProvider>
       <BrowserRouter>
         <Routes>
-          <Route element={<ProtectedRoute />}>
+          <Route element={<HomeProtection />}>
             <Route path="/home" element={<Home />} />
           </Route>
           <Route path="/" element={<Login />} />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='navbar'>
       <span className="logo">Lama Chat</span>
       <div className="user">
+        <button onClick={() => {
+          signOut(auth)
+          navigate("/");
+        }} >Logout</button>
       </div>
     </div>
   );
