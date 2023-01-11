a940ce80952dd01dbd565175ea44593ad79176a1
altarulas <altarulas09@gmail.com>
2023-01-11 19:48:03 +0300

    commit: add uuid package

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
 
-const Message = () => {
+import { AuthContext } from "../Context/Auth";
+import { ChatContext } from "../Context/Chat";
+
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div>
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
       <div className="messageInfo">
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        />
         <span>just now</span>
       </div>
       <div className="messageContent">
-        <p></p>
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
   );
