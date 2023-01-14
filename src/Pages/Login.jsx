22e5897b5393c74a5a892478b0a67c06b86610e9
altarulas <altar@brewww.com>
2023-01-14 17:31:26 +0300

    commit: add tailwind to the project and refactor login page with new css

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,19 +1,26 @@
+import { Button, TextField } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 
+import Loading from "../Utility/Loading"
 import { auth } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    });
 
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
+    const loginHandler = async (e) => {
         e.preventDefault();
-        const email = e.target[0].value;
-        const password = e.target[1].value;
+
+        const email = user.email
+        const password = user.password
 
         try {
             setLoading(true);
@@ -23,26 +30,61 @@ const Login = () => {
             setLoading(false);
             setError(true);
         }
+
+        setUser({ ...user, email: "" });
+        setUser({ ...user, password: "" });
         setLoading(false);
     };
     return (
-        <div className="formContainer">
-            <div className="formWrapper">
-                <span className="logo">Chat App Login</span>
-                <form onSubmit={handleSubmit}>
-                    <input type="email" placeholder="email" />
-                    <input type="password" placeholder="password" />
-                    <button>LOGIN</button>
-                    {loading && "Please wait..."}
-                    {error && <span>Something went wrong</span>}
-                </form>
-                <p>
-                    You don't have an account?
-                    <Link to="/register" style={{ textDecoration: "none", marginLeft: "7px" }} >Click Here</Link>
-                </p>
+        <div id="login-base" className="w-screen h-screen bg-blue-300 flex items-center justify-center">
+            <div id="login-form" className="w-108 bg-white rounded-md p-4 max-sm:w-80">
+                <div id="form-wrapper" className="flex flex-col w-full h-full px-16">
+                    <span className="text-center font-bold text-3xl my-4">
+                        Chat Application
+                    </span>
+                    <TextField
+                        margin="normal"
+                        id="standard-basic-2"
+                        label="E-mail"
+                        variant="standard"
+                        onChange={(e) => {
+                            setUser({ ...user, email: e.target.value });
+                        }}
+                    />
+                    <TextField
+                        margin="normal"
+                        id="password-standard-basic-3"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        variant="standard"
+                        onChange={(e) => {
+                            setUser({ ...user, password: e.target.value });
+                        }}
+                    />
+                    <Button
+                        onClick={loginHandler}
+                        style={{ marginTop: "28px" }}
+                        variant="contained">
+                        LOGIN
+                    </Button>
+                    <span className="text-base mt-8 mb-4 text-center">
+                        Don't you have an account?
+                        <Link
+                            className="font-bold"
+                            to="/register"
+                            style={{ textDecoration: "none", marginLeft: "7px" }}>
+                            Click Here
+                        </Link>
+                    </span>
+                    {loading && <div id="svg-wrapper" className="my-4 flex justify-center">
+                        <Loading />
+                    </div>}
+                    {error && <span id="error-wrapper" className="text-lg font-semibold my-4 flex justify-center text-red-600">
+                        Login Failed Try Again
+                    </span>}
+                </div>
             </div>
-            <span className="logo">CI/CD is working!!!</span>
-            <span className="logo">CI/CD is working...</span>
         </div>
     );
 };
