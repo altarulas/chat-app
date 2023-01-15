7a3b51f097a92c78b51b1e18beefb9e324720510
altarulas <altarulas09@gmail.com>
2023-01-15 18:10:39 +0300

    chore: remove unnecessary context and imports

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,10 +1,8 @@
-import { ChatContext } from "../Context/Chat";
 import Input from "./Input";
 import Messages from "./Messages";
-import { useContext } from "react";
 
 const Chat = () => {
-    const { data } = useContext(ChatContext);
+
     return (
         <div id="chat-base" className=" w-2/3 h-full rounded-r-lg flex flex-col">
             <Messages />
