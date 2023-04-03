b912bde085a816639b15e2b8e054c366a0547aa0
altarulas <altarulas09@gmail.com>
2023-04-03 16:19:33 +0300

    commit: remove dummy text

--- src/Pages/AppScreen.jsx
+++ src/Pages/AppScreen.jsx
@@ -1,8 +1,6 @@
 import Chat from "../Components/Chat"
 import SideBar from "../Components/SideBar"
 
-// dummy text
-
 const AppScreen = () => {
     return (
         <div id="app-base" className="w-screen h-screen bg-indigo-500 flex items-center justify-center">
